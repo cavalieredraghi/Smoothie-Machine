2e99c74ab096de2a2d225d98d982e2059a3fa3b4
Anthony Holt <Anthony Holt>
2016-11-02 14:14:23 -0400

    Adding some File i/o functionallity, may be my last commit as I am now trying to incorporate this program into my User Application for 1150

--- src/day2/fruitsSuperclass/Shoppe.java
+++ src/day2/fruitsSuperclass/Shoppe.java
@@ -3,6 +3,8 @@ package day2.fruitsSuperclass;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -10,35 +12,47 @@ import java.util.Collection;
 import java.util.Scanner;
 
 public class Shoppe {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ReadRecipes readRecipes = new ReadRecipes();
 		// add a second file for the price of the products allow it to be read
 		// and in able it to be before the ingredients
 		final Collection<SimpleRecipe> recipes = readRecipes.loadRecipes("Recipes.csv");
 		final Collection<SimpleRecipe> prices = readRecipes.loadRecipes("RecipePrice.csv");
+		final Collection<SimpleRecipe> ingredients = readRecipes.loadRecipes("Ingredients.csv");
 
 		Scanner input = new Scanner(System.in);
 		String option;
 		System.out.println("Welcome to Smooth Moves: What Smoothie would you like to day?\n");
 		while (true) {
 
-			System.out.println("Please select from the list of options: \n" + "1) Add items to cart\n" + "2) check out\n"
-					+ "3) recipe prices\n" + "4) recipes ingredients list\n" + "x) to exit.");
+			System.out.println("Please select from the list of options: \n" + "1) create custom smoothie\n"
+					+ "2) recipe prices\n" + "3) recipes ingredients list\n" + "x) to exit.");
 			System.out.print("> ");
 			option = input.nextLine();
+			
 			if (option.equals("1")) {
-
+				for (SimpleRecipe Ingredient : ingredients) {
+				System.out.println(Ingredient.getName()+": Price: " + Ingredient.getIngredients());
+				}
+				File dir = new File ("/user/bigge/workspace/Smoothie");
+				File file = new File ("custom_smoothie.cvs");
+				
+				FileOutputStream outFile = new FileOutputStream(file);
+				String s;
+				do{
+					System.out.println("To escape just hit enter on a blank line.");
+					System.out.print("> ");
+					s = input.nextLine();
+				outFile.write(s.getBytes());
+				}while (!"".equals(s));
 			}
 			if (option.equals("2")) {
-
-			}
-			if (option.equals("3")) {
 				for (SimpleRecipe price : prices) {
 					System.out.println(price.getName());
 					System.out.println("Price: " + price.getIngredients());
 				}
 			}
-			if (option.equals("4")) {
+			if (option.equals("3")) {
 				for (SimpleRecipe recipe : recipes) {
 					System.out.println(recipe.getName());
 					System.out.println(recipe.getIngredients());
--- src/day2/fruitsSuperclass/ShoppingCart.java
+++ src/day2/fruitsSuperclass/ShoppingCart.java
@@ -1,33 +1,33 @@
-package day2.fruitsSuperclass;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Scanner;
-
-public class ShoppingCart {
-	Scanner input = new Scanner(System.in);
-	String option;
-	ReadRecipes readRecipes = new ReadRecipes();
-	private Collection<ShoppingCart> cart = new ArrayList();
-	final Collection<SimpleRecipe> prices = readRecipes.loadRecipes("RecipePrice.csv");
-	
-	public void addProduct() {
-		while (true) {
-			System.out.println("Which Smoothie would you like to purchase?");
-			System.out.print("> ");
-			// may need a loop while loop most likely best foreach loop then add in 
-			//if coditionals you will need 3 foreach loops all linked to one another 
-			//through user input.
-			while(true){
-			option = input.nextLine();
-			 for(i=1; 1>)
-			}
-		}
-
-	}
-
-	public void cartLocation() {
-		
-		
-	}
-}
+//package day2.fruitsSuperclass;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.Scanner;
+//
+//public class ShoppingCart {
+//	Scanner input = new Scanner(System.in);
+//	String option;
+//	ReadRecipes readRecipes = new ReadRecipes();
+//	private Collection<ShoppingCart> cart = new ArrayList();
+//	final Collection<SimpleRecipe> prices = readRecipes.loadRecipes("RecipePrice.csv");
+//	
+//	public void addProduct() {
+//		while (true) {
+//			System.out.println("Which Smoothie would you like to purchase?");
+//			System.out.print("> ");
+//			// may need a loop while loop most likely best foreach loop then add in 
+//			//if coditionals you will need 3 foreach loops all linked to one another 
+//			//through user input.
+//			while(true){
+//			option = input.nextLine();
+//			 for(i=1; 1>)
+//			}
+//		}
+//
+//	}
+//
+//	public void cartLocation() {
+//		
+//		
+//	}
+//}
